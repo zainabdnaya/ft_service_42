34b1e5934dc661b9fdab13c083b94af2847d9a0c
zainabdnaya <diyanazizo13@gmail.com>
2021-03-25 19:12:57 +0100

    mysql

--- nginx/srcs/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-openrc
-touch /run/openrc/softlevel
-service nginx start
-/bin/sh
--- wordpress/srcs/setup_wdp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-openrc
-touch /run/openrc/softlevel
-service php-fpm7 start
-service nginx start
-/bin/sh
