ccbdd19574d803892a0f1d9ce257012232d74849
zainabdnaya <diyanazizo13@gmail.com>
2021-03-27 18:19:30 +0100

    graphana

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ minikube start
 eval $(minikube -p minikube docker-env)
 
 echo " ============ BUILDING IMAGES ============ "
-# docker build -t nginx:service nginx/
+docker build -t nginx:service nginx/
 docker build -t phpmyadmin:service phpmyadmin/
 docker build -t wordpress:service wordpress/
 docker build -t mysql:service ./mysql
@@ -15,8 +15,8 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manife
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f yaml/metallb.yaml
 kubectl apply -f yaml/mysql_pvc.yaml
-# kubectl apply -f yaml/nginx.yaml
-# kubectl apply -f yaml/nginx_service.yaml
+kubectl apply -f yaml/nginx.yaml
+kubectl apply -f yaml/nginx_service.yaml
 kubectl apply -f yaml/php.yaml
 kubectl apply -f yaml/php_service.yaml
 kubectl apply -f yaml/wordpress.yaml
