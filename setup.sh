4859282892601eb9032a51ef88dd06e37a849c90
zainabdnaya <diyanazizo13@gmail.com>
2021-03-30 12:02:51 +0100

    READY

--- setup.sh
+++ setup.sh
@@ -4,32 +4,32 @@ minikube start
 eval $(minikube -p minikube docker-env)
 
 echo " ============ BUILDING IMAGES ============ "
-docker build -t nginx:service nginx/
-docker build -t phpmyadmin:service phpmyadmin/
-docker build -t wordpress:service wordpress/
-docker build -t mysql:service ./mysql
-docker build -t ftps:service ./ftps
-docker build -t grafana:service ./grafana
-docker build -t influxdb:service ./influxdb
+docker build -t nginx:service ./srcs/nginx/
+docker build -t phpmyadmin:service ./srcs/phpmyadmin/
+docker build -t wordpress:service ./srcs/wordpress/
+docker build -t mysql:service ./srcs/mysql/
+docker build -t ftps:service ./srcs/ftps/
+docker build -t grafana:service ./srcs/grafana/
+docker build -t influxdb:service ./srcs/influxdb/
 echo " ============ KUBERNETES ============ "
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f yaml/metallb.yaml
-kubectl apply -f yaml/mysql_pvc.yaml
-# kubectl apply -f yaml/influxdb_pvc.yaml
-kubectl apply -f yaml/nginx.yaml
-kubectl apply -f yaml/nginx_service.yaml
-kubectl apply -f yaml/php.yaml
-kubectl apply -f yaml/php_service.yaml
-kubectl apply -f yaml/wordpress.yaml
-kubectl apply -f yaml/wordpress_service.yaml
-kubectl apply -f yaml/mysql.yaml
-kubectl apply -f yaml/mysql_service.yaml
-kubectl apply -f yaml/ftps.yaml
-kubectl apply -f yaml/ftps_service.yaml
-kubectl apply -f yaml/grafana.yaml
-kubectl apply -f yaml/grafana_service.yaml
-kubectl apply -f yaml/influxdb.yaml
-kubectl apply -f yaml/influxdb_service.yaml
+kubectl apply -f srcs/yaml/metallb.yaml
+kubectl apply -f srcs/yaml/mysql_pvc.yaml
+kubectl apply -f srcs/yaml/influxdb_pvc.yaml
+kubectl apply -f srcs/yaml/nginx.yaml
+kubectl apply -f srcs/yaml/nginx_service.yaml
+kubectl apply -f srcs/yaml/php.yaml
+kubectl apply -f srcs/yaml/php_service.yaml
+kubectl apply -f srcs/yaml/wordpress.yaml
+kubectl apply -f srcs/yaml/wordpress_service.yaml
+kubectl apply -f srcs/yaml/mysql.yaml
+kubectl apply -f srcs/yaml/mysql_service.yaml
+kubectl apply -f srcs/yaml/ftps.yaml
+kubectl apply -f srcs/yaml/ftps_service.yaml
+kubectl apply -f srcs/yaml/grafana.yaml
+kubectl apply -f srcs/yaml/grafana_service.yaml
+kubectl apply -f srcs/yaml/influxdb.yaml
+kubectl apply -f srcs/yaml/influxdb_service.yaml
 
---
+++ srcs/mysql/srcs/stop.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+printf "READY\n";
+
+while read line; do
+  kill -3 $(cat "/var/run/supervisord.pid")
+done < /dev/stdin
